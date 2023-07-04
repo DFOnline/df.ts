559bc9bdb634a72b3011994330ea6c877d303089
GeorgeRNG <grogrng@gmail.com>
2023-07-04 11:50:11 +0100

    Add some actiondump stuff to the index

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # df.ts
-Classes and wrappers for DF data for use in node.js or the web.
+Classes and wrappers for DF data for use in node.js or the web.  
+
+Install: `npm i git+https://github.com/DFOnline/df.ts.git`
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import 'reflect-metadata';
 
+import ActionDump from './actiondump/actiondump';
+import CodeBlock from './actiondump/codeblock';
+
 import Template from "./template/template";
 import { Block, Bracket, ArgumentBlock, ActionBlock, TemplateBlock, DataBlock, Else, SelectionBlock, SubActionBlock } from "./template/block";
 import { SecondLineBlock, ThirdLineBlock, ForthLineBlock } from "./template/lines";
 
-export default Template;
+export { Template };
 export { Block, Bracket, ArgumentBlock, ActionBlock, TemplateBlock, DataBlock, Else, SelectionBlock, SubActionBlock };
 export { SecondLineBlock, ThirdLineBlock, ForthLineBlock };
+
+export { ActionDump, CodeBlock }
